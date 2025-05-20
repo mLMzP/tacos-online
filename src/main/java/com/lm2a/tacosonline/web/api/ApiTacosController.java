c8efbdc45949e2aa05397f28ac725c0e064f13b1
lm2a <mario.lamenza@gmail.com>
2025-05-20 14:07:15 +0200

    Septimo commit  (REST)

---
+++ src/main/java/com/lm2a/tacosonline/web/api/ApiTacosController.java
@@ -0,0 +1,49 @@
+package com.lm2a.tacosonline.web.api;
+
+
+import com.lm2a.tacosonline.data.TacoRepository;
+import com.lm2a.tacosonline.model.Taco;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path="/api", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class ApiTacosController {
+
+    @Autowired
+    TacoRepository tacoRepository;
+
+//    @GetMapping("/tacos/{id}")
+//    public Taco tacoById(@PathVariable("id") Long id){
+//        Optional<Taco> optTaco = tacoRepository.findById(id);
+//        if(optTaco.isPresent()){
+//            return optTaco.get();
+//        }
+//        return null;
+//    }
+
+    @GetMapping("/tacos/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id){
+        Optional<Taco> optTaco = tacoRepository.findById(id);
+        if(optTaco.isPresent()){
+            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping(path="/tacos", consumes="application/json")
+    public ResponseEntity<Taco> saveTaco(@RequestBody Taco taco){
+        Taco savedTaco = tacoRepository.save(taco);
+        if(savedTaco != null){
+            return new ResponseEntity<>(savedTaco, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+
+}
